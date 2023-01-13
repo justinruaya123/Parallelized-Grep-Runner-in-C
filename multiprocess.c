68c99144c214f9020ab8a9596cdb1ebad6862ab0
justinruaya123 <justinruaya123@github.com>
2023-01-13 22:56:38 +0800

    Fixed race conditions on multiprocess

--- multiprocess.c
+++ multiprocess.c
@@ -8,9 +8,8 @@
 
 #include<limits.h>
 #include<assert.h>
-// Multithreaded stuff
-#include<pthread.h>
-#include<semaphore.h>
+
+#include<sys/wait.h>
 
 // ************** FUNCTION POINTERS **************
 
@@ -22,13 +21,13 @@ typedef struct __node_t {
 typedef struct __queue_t {
     node_t *head;
     node_t *tail;
-    pthread_mutex_t head_lock, tail_lock;
+    // init mutex head_lock and tail_lock
 } queue_t;
 // Auxiliary functions
-void accessDir(char [], char [], queue_t *, int);
+int accessDir(char [], char [], queue_t *, int);
 int isFile(char []);
 int getGrepMatch(char [], char []);
-void *work(void *);
+void work(queue_t *, char [], int);
 
 void Queue_Init(queue_t *);
 void Queue_Enqueue(queue_t *, char []);
@@ -37,79 +36,42 @@ void Queue_Free(queue_t *);
 
 // ************** CONSTANTS **************
 const char slash[] = "/";
-pthread_t tid[8];
-sem_t sem_work;
-int done = 0;
-int N = 1;
-// ************** THREADING SHENANIGANS **************
-int counter = 0;
-pthread_mutex_t m_counter = PTHREAD_MUTEX_INITIALIZER;
-
-struct work_args {
-    queue_t *q;
-    char word[64];
-    int worker_id;
-} work_args;
-
-struct work_args args[8];
-
-
-// ****************************************************************************************************
-//                                    DRIVER CODE / MAIN
-// ****************************************************************************************************
+// ************** MAIN CODE **************
 int main(int argc, char *argv[]) {
-    N = atoi(argv[1]);
+    int N = atoi(argv[1]);
+
     queue_t *QUEUE = malloc(sizeof(queue_t));
     Queue_Init(QUEUE);
-    sem_init(&sem_work, 0, 0);
     
     char *path = realpath(argv[2], NULL);
-    
     Queue_Enqueue(QUEUE, path);
-    //Create threads and execute code
+    setvbuf(stdout, NULL, _IONBF, 0);
     for(int x = 0; x < N; x++) {
-        args[x].q = QUEUE;
-        strcpy(args[x].word, argv[3]);
-        args[x].worker_id = x;
-        pthread_create(&tid[x], NULL, work, (void *)&args[x]);
+        if(fork() == 0) {
+            work(QUEUE, argv[3], x);
+        }
     }
+
     for(int x = 0; x < N; x++) {
-        pthread_join(tid[x], NULL);
+        wait(NULL);
     }
-    sem_destroy(&sem_work);
+
     free(path);
     Queue_Free(QUEUE);
     return 0;
 }
-
-void *work(void *values) {
-    struct work_args *args = values;
-    while(1) {
-        sem_wait(&sem_work);
-        if(done) {
-            break;
-        }
-        char file[PATH_MAX];
-        Queue_Dequeue(args->q, file);
-        accessDir(file, args->word, args->q, args->worker_id);
-        pthread_mutex_lock(&args->q->head_lock);
-        pthread_mutex_lock(&args->q->tail_lock);
-        if(args->q->head == args->q-> tail) {
-            pthread_mutex_lock(&m_counter);
-            if(counter == 0) {
-                done = 1;
-                for(int x = 0; x < N; x++) // flush standby threads
-                    sem_post(&sem_work);
-            }
-            pthread_mutex_unlock(&m_counter);
-        }
-        pthread_mutex_unlock(&args->q->head_lock);
-        pthread_mutex_unlock(&args->q->tail_lock);
-    }
-}
 // ****************************************************************************************************
 //                                       FILE SYSTEM/GREP FUNCTIONS
 // ****************************************************************************************************
+
+void work(queue_t *q, char word[], int worker_id) {
+    while(q->head != q-> tail) {
+        char file[PATH_MAX];
+        Queue_Dequeue(q, file);
+        accessDir(file, word, q, worker_id);
+    }
+    
+}
 
 // From https://stackoverflow.com/questions/4553012/checking-if-a-file-is-a-directory-or-just-a-file
 int isFile(char path[]) {
@@ -125,13 +87,10 @@ int getGrepMatch(char file_path[], char word[]) {
 }
 
 
-void accessDir(char path[], char word[], queue_t *q, int worker_id) {
+int accessDir(char path[], char word[], queue_t *q, int worker_id) {
     struct dirent *child; //can be a directory or just a file
     char filename[PATH_MAX];
     DIR *dir = opendir(path);
-    pthread_mutex_lock(&m_counter);
-    counter++;
-    pthread_mutex_unlock(&m_counter);
     printf("[%d] DIR %s\n", worker_id, path);
 
     while ((child = readdir(dir)) != NULL) {
@@ -150,10 +109,8 @@ void accessDir(char path[], char word[], queue_t *q, int worker_id) {
             Queue_Enqueue(q, filename);
         }
     }
-    pthread_mutex_lock(&m_counter);
-    counter--;
-    pthread_mutex_unlock(&m_counter);
     closedir(dir);
+    return 0;
 }
  
 // ****************************************************************************************************
@@ -165,8 +122,8 @@ void Queue_Init(queue_t *q) {
     node_t *tmp = malloc(sizeof(node_t));
     tmp->next = NULL;
     q->head = q->tail = tmp;
-    pthread_mutex_init(&q->head_lock, NULL);
-    pthread_mutex_init(&q->tail_lock, NULL);
+    // init head
+    // init tail
 }
 
 // CS 140 2223A - Lecture 16
@@ -175,31 +132,29 @@ void Queue_Enqueue(queue_t *q, char file[]) {
     assert(tmp!= NULL);
     strcpy(tmp->file, file);
     tmp->next = NULL;
-    pthread_mutex_lock(&q->tail_lock);
+    // lock tail
     q->tail->next = tmp;
     q->tail = tmp;
-    pthread_mutex_unlock(&q->tail_lock);
-    sem_post(&sem_work);
+    // unlock tail
 }
 
 // CS 140 2223A - Lecture 16
 int Queue_Dequeue(queue_t *q, char file[]) {
-    pthread_mutex_lock(&q->head_lock);
+    //lock head
     node_t *tmp = q->head;
     node_t *new_head = tmp->next;
     if(new_head == NULL) {
-        pthread_mutex_unlock(&q->head_lock);
+        //unlock head
         return -1;
     }
     strcpy(file, new_head->file);
     q->head = new_head;
-    pthread_mutex_unlock(&q->head_lock);
+    //unlock head
     free(tmp);
     return 0;
 }
 
 // https://stackoverflow.com/questions/6417158/c-how-to-free-nodes-in-the-linked-list
-// To be executed ONLY on the main tread, after the threads are waiting.
 void Queue_Free(queue_t *q) {
     node_t *tmp = q->head;
     while(q->head != q->tail) {
